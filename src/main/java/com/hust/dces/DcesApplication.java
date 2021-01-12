08d1a064c7b87c90eb2da280a29fe5240aef570d
kls64 <1370905044@qq.com>
2021-01-12 15:01:10 +0800

    Initial commit

---
+++ src/main/java/com/hust/dces/DcesApplication.java
@@ -0,0 +1,13 @@
+package com.hust.dces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hust/dces/DcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.dces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
