7349d995cdd80b34859f70bb1ed28d89e4b93248
hustwzk <2671903645@qq.com>
2021-01-19 21:32:51 +0800

    已实现各个页面的正常运行和跳转

--- src/main/java/com/hust/dces/Controller/UserController.java
+++ src/main/java/com/hust/dces/Controller/UserController.java
@@ -11,6 +11,83 @@ public class UserController {
     // http://localhost:8080/user/login
     @GetMapping("/login")
     public String userlogin(){
+
         return "login";   // login.html
+
     }
+
+    @GetMapping("/register")
+    public String userregister(){
+
+        return "register";  // register.html
+    }
+
+    @GetMapping("/forgot-password")
+    public String userfp(){
+
+        return "forgot-password"; // forgot-password.html
+
+    }
+
+    @GetMapping("/index")
+    public String userindex(){
+
+        return "index"; // index.html
+    }
+
+    @GetMapping("/indexadmin")
+    public String adminindex(){
+
+        return "indexadmin"; // indexadmin.html
+    }
+
+    @GetMapping("/analyze")
+    public String fileanalyze(){
+
+        return "analyze"; // analyze.html
+    }
+
+    @GetMapping("/appeal")
+    public String userappeal(){
+
+        return "appeal"; // appeal.html
+    }
+
+    @GetMapping("/personalinfo")
+    public String userinfo(){
+
+        return "personalinfo"; // personalinfo.html
+    }
+
+    @GetMapping("/review")
+    public String userreview(){
+
+        return "review"; // review.html
+    }
+
+    @GetMapping("/ssemanage")
+    public String adminsmanage(){
+
+        return "ssemanage"; // ssemanage.html
+    }
+
+    @GetMapping("/sseresolv")
+    public String adminsresolv(){
+
+        return "sseresolv"; // sseresolv.html
+    }
+
+    @GetMapping("/submitfile")
+    public String usersf(){
+
+        return "submitfile"; // submitfile.html
+    }
+
+    @GetMapping("/update")
+    public String userupdate(){
+
+        return "update"; // update.html
+    }
+
+
 }
