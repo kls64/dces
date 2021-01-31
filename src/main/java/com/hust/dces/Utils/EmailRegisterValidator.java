0125c077195680b285ef9a0b4e3e5c8db5bdab09
MVP <lovetsw@foxmail.com>
2021-01-31 20:24:38 +0800

    1.登陆界面实现保存密码功能，将用户名和密码保存到客户端cookie，下次登录可以记住用户名和密码直接登录； 2.在登录界面将普通用户登录和管理员登录操作分开，用户即使输入正确的用户名和密码但用户类型选择不对依然登录失败； 3.登录失败之后不再重定向到一个新界面，由后端返回一个带参数的url,前端js判别给出提示； 4.在注册上使用js在前端判别输入的手机号和密码以及确认密码的合规性，之后再post到后端注册 5.给邮箱发验证码的注册的功能尚在开发中

--- src/main/java/com/hust/dces/Utils/EmailRegisterValidator.java
+++ src/main/java/com/hust/dces/Utils/EmailRegisterValidator.java
@@ -1,10 +1,38 @@
 package com.hust.dces.Utils;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public final class EmailRegisterValidator {
 
-    private byte[] generateVerificationCode(byte digits) // 生成
-    {
-        return null;
+    static private byte[] verificationCode = null;
+
+    public EmailRegisterValidator(int digits) {
+        byte[] verificationCode = new byte[digits];
+    }
+
+    // 随机生成 digits 个数字验证码
+    private static void generateVerificationCode() {
+        long timestamp = System.currentTimeMillis();
+        Random random = new Random(timestamp);
+        for (int i = 0; i < verificationCode.length; ++i) {
+            verificationCode[i] = (byte) random.nextInt(10);
+            random.setSeed(random.nextInt());
+        }
+    }
+
+    // 将随机生成的验证码发送到指定邮箱
+    private static void sendVerificationCodeToEmail(String email) {
+
+    }
+
+    public static void main(String args[]) {
+        System.out.println("this is main");
+        EmailRegisterValidator erv = new EmailRegisterValidator(6);
+        for (int i = 0; i < 1000; ++i) {
+            erv.generateVerificationCode();
+            System.out.println(Arrays.toString(verificationCode));
+        }
     }
 
 }
