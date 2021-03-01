c544b3114d374f417c4687d49bed143acba848e2
hustwzk <2671903645@qq.com>
2021-03-01 14:33:36 +0800

    实现文档关系图谱的展示

--- src/main/java/com/hust/dces/Controller/DocumentController.java
+++ src/main/java/com/hust/dces/Controller/DocumentController.java
@@ -4,13 +4,16 @@ import com.hust.dces.Entity.Document;
 import com.hust.dces.Entity.User;
 import com.hust.dces.Service.DocumentService;
 import com.hust.dces.Utils.FileUtil;
+import com.hust.dces.Utils.JavaRunPython;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.File;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Date;
 
@@ -22,7 +25,7 @@ public class DocumentController {
     private DocumentService documentService;
 
     @PostMapping("/upload")
-    public String fileUpload(HttpServletRequest request, @RequestParam("myFile") MultipartFile file) {
+    public String fileUpload(HttpServletRequest request, @RequestParam("myFile") MultipartFile file) throws IOException {
         //提交文件保存到后台并分析
         // 1.保存文件到硬盘上 利用工具类
         String fileName = file.getOriginalFilename();
@@ -47,10 +50,11 @@ public class DocumentController {
         document.setUserid(user.getUserid());
         documentService.addDoc(document); // 将文档信息保存到数据库中
         // 这里将上传的文档信息保留在session中，便于后续操作
-//        HttpSession session = request.getSession();
-//        session.setAttribute("currentDocument", document);
+        HttpSession session = request.getSession();
+        session.setAttribute("currentDocument", document);
         // 这里docid传不进session中，因为刚把一个新的 document 写进数据库中，还没有生成 docid。
         // 这里可以不将 docid 放进 Session 中
+        // JavaRunPython.textGraph();
         Integer newDocId = document.getDocid();
         return "redirect:/document/analyze/" + newDocId;
     }
@@ -58,6 +62,7 @@ public class DocumentController {
     @GetMapping("/analyze/{docid}") // 这里可以写@PostMapping("/submitfile")?
     public String fileAnalyze(@PathVariable("docid") Integer docid) {
         // 这里将来要写具体的分析业务，根据传进来的 docid 来查找具体的分析结果
+        JavaRunPython.textGraph();
         System.out.println(docid);
         return "analyze"; // analyze.html
     }
@@ -74,6 +79,12 @@ public class DocumentController {
         FileUtil.deleteFile(delDoc.getDocpath());
         documentService.deleteDocumentByDocid(docid);
         return "redirect:/user/index";
+    }
+
+    @GetMapping("/graph_show")
+    public String fileAnalyze(){
+
+        return "graph_show"; // graph_show.html
     }
 
 }
